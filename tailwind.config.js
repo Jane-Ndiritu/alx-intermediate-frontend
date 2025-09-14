5da45e801bbbf4c5849ad94e3eed707615e263b6
Jane-Ndiritu <jayndiritu36@gmail.com>
2025-09-14 23:20:28 +0300

    Setting Up and Installing Tailwind CSS with Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#339941", // Green
+        "primary-dark": "#627063", // Muted Green
+        secondary: "#712F22", // Dark Red/Brown
+        "secondary-dark": "#8C3421", // Dark Orange/Brown
+        accent: "#39984A", // Bright Green
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tailwind-scrollbar")],
+};
